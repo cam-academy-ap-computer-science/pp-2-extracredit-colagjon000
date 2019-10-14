e869f4bc6a1a394337a1d69a730490eef3ba62ce
Jonah Colagross <colagjon000@battlegroundps.org>
2019-10-14 07:50:55 -0700

    Basic Outline plan

--- src/ExtraCredit.java
+++ src/ExtraCredit.java
@@ -1,4 +1,22 @@
-
+/* Outline --
+ * Default size value = 3
+ * 	- Capsule and Nossle are the same
+ * 		- Default size = 3. Size * 2 - 1
+ * 		- StarValue always stays the same
+ * 		- Ending slashValue = size * 2 - 1
+ * 	- In between Spacers
+ * 		- internal "+=" = SIZE * 2
+ * 		- close with "+"
+ *  - internal modules -- 
+ *  	- Split between topArrows and BottomArrows
+ *  		- TopArrows -- Start Values
+ *  			- Height proportional to size
+ *  			- Edgedots = Size - 1
+ *  			- MiDots = Size + 1
+ *  			- /\ combination ++ per level
+ *  		- BottomArrows -- Start Values
+ *  			- Height Proportional to size
+ * */
 public class ExtraCredit {
 
 	public static void main(String[] args) {
